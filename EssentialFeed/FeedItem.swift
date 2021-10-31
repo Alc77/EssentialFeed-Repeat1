1c5d384edfef542988d36e3b75dc3624c05b185a
Alex Korabl'ov <akorablev77@gmail.com>
2021-10-31 18:38:53 +0200

    added the FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Alex Korablyov on 31.10.2021.
+//
+
+import Foundation
+ 
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
